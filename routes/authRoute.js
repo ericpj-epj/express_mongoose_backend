464f1d6b423366b8ecac651c19f17600715ee3c4
ericpj-epj <eric.jaucian@cpgradar.com>
2026-03-05 21:57:21 +0800

    Added sanitizebody to authRoute, added adminrequire on member route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,12 +5,17 @@ import {
   signUp,
   resetPassword,
 } from "../controller/authController.js";
-
+import { adminRequired } from "../middleware/authUtils.js";
+import { sanitizeBody } from "../middleware/sanitizeBody.js";
 const router = express.Router();
 
-router.post("/request-otp", requestOtp);
-router.post("/sign-up", signUp);
-router.post("/sign-in", signIn);
-router.post("/reset-password", resetPassword);
+router.post("/request-otp", sanitizeBody(["email", "purpose"]), requestOtp);
+router.post("/sign-up", sanitizeBody(["email", "password", "otp"]), signUp);
+router.post("/sign-in", sanitizeBody(["email", "password"]), signIn);
+router.post(
+  "/reset-password",
+  sanitizeBody(["email", "otp", "new_password"]),
+  resetPassword,
+);
 
 export default router;
--- routes/memberRoute.js
+++ routes/memberRoute.js
@@ -6,13 +6,13 @@ import {
   getMemberProfile,
   getCurrentMemberAccess,
 } from "../controller/memberController.js";
-
+import { adminRequired } from "../middleware/authUtils.js";
 const router = express.Router();
 
-router.patch("/:id", updateMemberStatus);
-router.post("/:id/resetPassword", resetMemberPassword);
-router.get("/:id/access", getMemberAccess);
-router.get("/me", getMemberProfile);
-router.post("/me/access", getCurrentMemberAccess);
+router.patch("/:id", adminRequired, updateMemberStatus);
+router.post("/:id/resetPassword", adminRequired, resetMemberPassword);
+router.get("/:id/access", adminRequired, getMemberAccess);
+router.get("/me", adminRequired, getMemberProfile);
+router.post("/me/access", adminRequired, getCurrentMemberAccess);
 // router.delete("/delete/:id", deleteMember);
 export default router;
